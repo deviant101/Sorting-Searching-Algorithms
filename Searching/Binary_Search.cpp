bb2b66d78b5eb519344400a03360d8707df30ddc
Farrukh Riaz <115878714+deviant101@users.noreply.github.com>
2023-10-05 00:13:51 +0500

    Binary Search

--- Searching/Binary_Search.cpp
+++ Searching/Binary_Search.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
 using namespace std;
 
+int BinarySearch(int *Array, int size, int target);
+
 int main()
 {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    int key = 0;
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+    cout << "Enter the key to be searched: ";
+    cin >> key;
+    int resutl = BinarySearch(arr, size, key);
+    if (resutl == -1)
+        cout << "The key is not present in the array";
+    else
+        cout << "The key is present at index " << resutl;
 
     return 0;
 }
 
 int BinarySearch(int *Array, int size, int target)
 {
-    int low = 0, high = size - 1;
+    int low = 0, high = size;
     while (low <= high)
     {
-
         int mid = (low + high) / 2;
         if (Array[mid] == target)
-            return true;
+            return mid;
         else if (Array[mid] < target)
             low = mid + 1;
         else
             high = mid - 1;
     }
-    return;
+    return -1;
 }
